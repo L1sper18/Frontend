d8ecabe14b845246cebbbec2972ee7c12ca05def
L1sper18 <sashamolod18@gmail.com>
2025-09-27 16:16:49 +0300

    1

---
+++ src/app/layout/layout.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './layout.html',
+  styleUrls:['layout.css']
+})
+export class Layout {
+title: string = 'Знижки в магазинах';
+Dodatok: string = 'App';
+}
